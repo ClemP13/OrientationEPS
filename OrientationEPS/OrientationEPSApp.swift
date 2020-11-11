f789b7f88d869f512be5b2c45a762d6d1d9afc35
clemp-applis <clement.pignet@hotmail.fr>
2020-11-11 15:13:43 +0100

    Initial Commit

---
+++ OrientationEPS/OrientationEPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientationEPSApp.swift
+//  OrientationEPS
+//
+//  Created by Clement Pignet on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct OrientationEPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
